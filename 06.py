1ae6ba2d34fb6f3fe1d14b663aede98050120f59
saf-sy <59076012+saf-sy@users.noreply.github.com>
2023-02-22 09:37:35 -0600

    Update 06.py

--- 06.py
+++ 06.py
@@ -2,18 +2,17 @@ import sys
 import string
 import math
 
-x = sys.stdin.readline().strip("/n")
+x = sys.stdin.readline().rstrip().strip()
 save = []
 magic = 0
 
-try:
+while x != "0":
     x = x.split(" ")
     xint = [int(s) for s in x]
     M = ((xint[0]**1+xint[1]**2)*((xint[2])**(1/2)/xint[3]))**(1/3)
     if M > magic:
-        save = xint
-    x = sys.stdin.readline().strip("/n")
-except:
-    print("{} Kindness, {} Stickers On Phone, {} Friends and {} Laughter produced the most Magic: {}".format(save[0], save[1], save[2], save[3], M))
+        save = x
+        magic = M
+    x = sys.stdin.readline().strip()
     
-
+print("{} Kindness, {} Stickers On Phone, {} Friends and {} Laughter produced the most Magic: {}".format(save[0], save[1], save[2], save[3], magic))
